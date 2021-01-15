596f87c2cd2bf9db297a9ef7c32917cb69dce98b
Drew R <drewr@Drews-MacBook-Pro.local>
2021-01-15 13:04:36 -0600

    no merge

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,13 @@
 
-// class App extends React.Component {
-//
-//     render = () => {
-//       return (
-//          <div>
-//           <h3>App page is working</h3>
-//         </div>
-//       )
-//     }
-// }
-//
-// ReactDOM.render(<App></App>, document.querySelector('main'))
+class App extends React.Component {
+
+    render = () => {
+      return (
+         <div>
+          <h3>App page is working</h3>
+        </div>
+      )
+    }
+}
+
+ReactDOM.render(<App></App>, document.querySelector('main'))
