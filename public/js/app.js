d7320d0159194c471a12e9b82882310b85b93b1c
Drew R <drewr@Drews-MacBook-Pro.local>
2021-01-15 17:04:15 -0600

    edit

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,16 @@
+// If else statment needed for boolean and checkboxes to be linked and data to be sent
+
 
 class App extends React.Component {
   state={
     name: "",
-
     vegtables:Boolean,
-
     meat:Boolean,
-
     topping: Boolean,
-
     image: "",
-
     taco:[]
   }
+
 
   handleChange = (event) => {
     this.setState({
@@ -20,6 +18,8 @@ class App extends React.Component {
     })
   }
 
+
+//Create
   create = event => {
     event.preventDefault();
     axios
@@ -36,6 +36,7 @@ class App extends React.Component {
     )
   }
 
+// Delete
   delete = event => {
   axios.delete('/taco/' + event.target.value).then(response => {
     this.setState({
@@ -53,6 +54,8 @@ class App extends React.Component {
     })
   }
 
+
+// Update
   update = event => {
   event.preventDefault()
   const id = event.target.id
@@ -71,15 +74,16 @@ class App extends React.Component {
 
     render = () => {
       return (
-         <div>
+      <div className='html'>
+        <h2>Whats on tuesdays menu</h2>
+        <div className='Menu'>
 
-          <h2>Whats on tuesdays menu</h2>
-          <form id ="makeTaco" onSubmit={this.create} >
+        <form id ="makeTaco" onSubmit={this.create} >
           <label htmlFor ="name">Taco:</label><br/>
-          <input type="text" name="name"id="name"/><br/>
+          <input type="text" name="name"id="name"     onChange={this.handleChange} value={this.state.name}/><br/>
 
-          <label htmlFor ="vegtables">vegtables:</label><br/>
-          <input type="checkbox"  name="vegtables"id="vegtables"/><br/>
+          <label htmlFor ="vegtables">Vegtables:</label><br/>
+          <input type="checkbox"  name="vegtables"id="vegtables"   onChange={this.handleChange} value={this.state.name}/><br/>
 
           <label htmlFor ="meat">Meat:</label><br/>
           <input type="checkbox" name="meat"id="meat"
@@ -89,10 +93,39 @@ class App extends React.Component {
           <input type="text" name="image"id="image"/><br/>
 
           <input type="submit" value='Add'/> <br/>
-          </form>
+        </form>
         </div>
+
+        <h2>Taco Results</h2>
+
+          <ul>
+            {this.state.taco.map((taco) => {
+              return (
+                <li key={taco._id}>
+                {taco.name} <br />
+
+                <img src={taco.image} alt={taco.name} />
+
+                <div className='Edit feature'>
+                <details>
+                <summary>Make Changes</summary>
+                <form id={taco.id} onSubmit={this.update}>
+
+                  <label htmlFor='name'>Name</label>
+                  <br />
+                  <input type='text' placeholder={taco.name} id='name' onChange={this.handleChange} />
+
+                </form>
+                </details>
+                </div>
+                </li>
+              )
+            })}
+          </ul>
+
+      </div>
       )
     }
-}
+  } // Closing Tag to whole React Fn.
 
 ReactDOM.render(<App></App>, document.querySelector('main'))
