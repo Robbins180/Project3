a4db07621244fae9ed52f3eeed8e0c5423846ea2
noel rodriguez <noelrodriguez@noels-MacBook.local>
2021-01-15 18:20:23 -0500

    merg

--- public/js/app.js
+++ public/js/app.js
@@ -1,136 +1,93 @@
-// If else statment needed for boolean and checkboxes to be linked and data to be sent
-
 
 class App extends React.Component {
   state={
     name: "",
+
     vegtables:Boolean,
-    meat:Boolean,
+
+     meat:Boolean,
+
     topping: Boolean,
+
     image: "",
+
     taco:[]
+
   }
-
-
-  handleChange = (event) => {
+  handleChage = (event) => {
     this.setState({
       [event.target.id]:event.target.value
     })
-  }
 
+}
 
-//Create
-  create = event => {
+  create = (event) => {
+     event.preventDefault();
+     axios.post('/taco',this.state).then((response) => {
+       console.log(response.data);
+       this.setState({
+
+         taco:response.data,
+
+         vegtables:Boolean,
+
+          meat:Boolean,
+
+         topping: Boolean,
+
+         image: "",
+
+ })
+
+update = (event)=>{
+    console.log(event.target.id)
     event.preventDefault();
     axios
-    .post('/taco', this.state)
-    .then(response =>
-    this.setState({
-      tacos: response.data,
-      name:'',
-      image:'',
-      vegtables: Boolean,
-      meat: Boolean,
-      topping: Boolean,
-      })
+        .put('/taco/'+event.target.id, this.state)
+        .then((response)=>{
+            console.log(response.data)
+            this.setState({
+              taco:response.data,
+
+              vegtables:Boolean,
+
+               meat:Boolean,
+
+              topping: Boolean,
+
+              image: "",
+
+
+            })
+        },
+        (error)=>{
+            console.log(error)
+        }
     )
-  }
-
-// Delete
-  delete = event => {
-  axios.delete('/taco/' + event.target.value).then(response => {
-    this.setState({
-      taco: response.data
-      })
-    })
-  }
-
-
-  componentDidMount = () => {
-    axios.get("/taco").then((response) => {
-      this.setState({
-        taco: response.data,
-      })
-    })
-  }
-
-
-// Update
-  update = event => {
-  event.preventDefault()
-  const id = event.target.id
-  axios.put('/taco/' + id, this.state).then(response => {
-    this.setState({
-      taco: response.data,
-      name: '',
-      image: '',
-      vegtables: Boolean,
-      meat: Boolean,
-      topping: Boolean,
-      })
-    })
-  }
+}
 
 
     render = () => {
       return (
-      <div className='html'>
-        <h2>Whats on tuesdays menu</h2>
-        <div className='Menu'>
+         <div>
 
-<<<<<<< HEAD
           <h2>Whats n tuesdays menu</h2>
           <form id ="makeTaco" onSubmit={this.create} >
-=======
-        <form id ="makeTaco" onSubmit={this.create} >
->>>>>>> c3faa1733f91552fd29e8dc31d9789eb24dc24a0
           <label htmlFor ="name">Taco:</label><br/>
-          <input type="text" name="name"id="name"     onChange={this.handleChange} value={this.state.name}/><br/>
+          <input type="text" name="name"id="name"/><br/>
 
-          <label htmlFor ="vegtables">Vegtables:</label><br/>
-          <input type="checkbox"  name="vegtables"id="vegtables"   onChange={this.handleChange} value={this.state.name}/><br/>
+          <label htmlFor ="vegtables">vegtables:</label><br/>
+          <input type="text"  name="vegtables"id="vegtables"/><br/>
 
           <label htmlFor ="meat">Meat:</label><br/>
-          <input type="checkbox" name="meat"id="meat"
-          onChange={this.handleChange} value={this.state.name}/><br/>
+          <input type="text" name="meat"id="meat"/><br/>
 
           <label htmlFor ="image">Image:</label><br/>
           <input type="text" name="image"id="image"/><br/>
-
-          <input type="submit" value='Add'/> <br/>
-        </form>
+          </form>
         </div>
-
-        <h2>Taco Results</h2>
-
-          <ul>
-            {this.state.taco.map((taco) => {
-              return (
-                <li key={taco._id}>
-                {taco.name} <br />
-
-                <img src={taco.image} alt={taco.name} />
-
-                <div className='Edit feature'>
-                <details>
-                <summary>Make Changes</summary>
-                <form id={taco.id} onSubmit={this.update}>
-
-                  <label htmlFor='name'>Name</label>
-                  <br />
-                  <input type='text' placeholder={taco.name} id='name' onChange={this.handleChange} />
-
-                </form>
-                </details>
-                </div>
-                </li>
-              )
-            })}
-          </ul>
-
-      </div>
       )
     }
-  } // Closing Tag to whole React Fn.
+}
 
 ReactDOM.render(<App></App>, document.querySelector('main'))
