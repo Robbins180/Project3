270b29d73a9dfd676e60bc9e9e4d5411d0ee94b5
Drew R <drewr@Drews-MacBook-Pro.local>
2021-01-14 18:48:56 -0600

    index update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Depdencies
 const express= require('express')
 const mongoose = require('mongoose')
 const app = express()
@@ -6,24 +7,29 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 
+// MiddleWare
 app.use(express.json())
 app.use(express.static('public'))
+
+//Controller Link
 const tacoController = require('./controllers/taco_controller.js')
 app.use('/taco', tacoController)
 
+
+// Random Mongo Stuff
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
 })
-
+// Error/
 mongoose.connection.on('error', (err) =>
   console.log(
     err.message,
     'is mongod not running/Problem with atlas connection?'
   )
 )
-
+// Connection/Disconnection
 mongoose.connection.on('connected', () =>
   console.log('mongo connected: ', MONGODB_URI)
 )
@@ -31,7 +37,7 @@ mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
 
 
-
+// Listener
 app.listen(PORT,() => {
   console.log(`listening on port${PORT}`);
 })
